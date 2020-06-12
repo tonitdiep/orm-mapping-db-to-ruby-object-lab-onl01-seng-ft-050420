5c7d064be3d11addc5375f8993eca1018e39f396
Toni  Diep <adiep123@gmail.com>
2020-06-12 03:52:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
       SELECT * FROM students WHERE students.grade = 10 LIMIT 1
     SQL
     
-    DB[:conn].execute(sql, grade).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
   end
