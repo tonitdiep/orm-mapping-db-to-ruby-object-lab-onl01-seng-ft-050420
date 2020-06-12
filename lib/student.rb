515de179adee08aa136cbe5974fd36509c5e87da
Toni  Diep <adiep123@gmail.com>
2020-06-12 03:27:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,10 @@ class Student
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
-
+  
+  def self.all_students_in_grade_9
+    
+  end
   def self.find_by_name(name)
      sql = <<-SQL
         SELECT * FROM students WHERE name = ? LIMIT 1
