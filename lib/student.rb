14bfb053073301e084d1726a22fdddceb16553cb
Toni  Diep <adiep123@gmail.com>
2020-06-12 03:31:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE students.grade = 9
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map
   end
   def self.find_by_name(name)
      sql = <<-SQL
