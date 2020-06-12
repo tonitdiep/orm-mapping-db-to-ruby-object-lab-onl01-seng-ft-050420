107de87c47c3a235861c5c1d1e989b45f8faff3e
Toni  Diep <adiep123@gmail.com>
2020-06-12 03:18:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ class Student
         SELECT * FROM student WHERE name = ? LIMIT 1
      SQL
      
-     DB[:conn].execute(sql, name)
+     DB[:conn].execute(sql, name).map do |row|
     # find the student in the database given a name
     # return a new instance of the Student class
   end
