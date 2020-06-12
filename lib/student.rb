3a48cdfeb2b2e8e22af1ec275e3cf937177841dc
Toni  Diep <adiep123@gmail.com>
2020-06-12 03:13:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,7 @@ class Student
 
   def self.find_by_name(name)
      sql = <<-SQL
-        SELECT 
+        SELECT * FROM student
      SQL
     # find the student in the database given a name
     # return a new instance of the Student class
