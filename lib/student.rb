0e6006009e5ec74e7507cfed99261d2b48efef09
Toni  Diep <adiep123@gmail.com>
2020-06-12 03:07:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,11 +10,13 @@ class Student
   end
 
   def self.all
+    
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
 
   def self.find_by_name(name)
+    
     # find the student in the database given a name
     # return a new instance of the Student class
   end
