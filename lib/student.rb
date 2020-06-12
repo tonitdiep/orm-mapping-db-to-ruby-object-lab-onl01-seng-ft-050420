ec04c594dcb5254baa78b06d270ab511e5a9b9fa
Toni  Diep <adiep123@gmail.com>
2020-06-12 03:54:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,6 +39,12 @@ class Student
     end.first
   end
   
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+    
+    SQL
+    
+  end
   def self.all_students_in_grade_9
     sql = <<-SQL
       SELECT * FROM students WHERE students.grade = 9
